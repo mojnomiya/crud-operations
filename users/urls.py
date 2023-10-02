300de38385c903851cbd49313b271d96aa4b59ac
mojnomiya <mearn4706@gmail.com>
2023-10-02 16:05:17 +0600

    created user model, serializer, and necessary views.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import UsersGetPostView, UserDetaiDeletelView
 
 urlpatterns = [
-
+    path('', UsersGetPostView.as_view(), name='users'),
+    path('<int:pk>/', UserDetaiDeletelView.as_view(), name='users_details'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import UserSerializer
+from .models import User
 
 # Create your views here.
+class UsersGetPostView(ListCreateAPIView):
+    """
+    /users/ - this API endpoint will return a list of all the user details if the request method is GET. By POST request to this endpoint a signle user can be created.
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class UserDetaiDeletelView(RetrieveUpdateDestroyAPIView):
+    """
+    /users/<id> - this API endpoint will return a  details of a single user with the id, if the request method is GET. By PUT request to this endpoint that user can be updated or deleted.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
